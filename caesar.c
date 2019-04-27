b2343ad3f6b63c62bfd5fd241a92fddb4aca236b
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-27 18:28:27 +0000

    began caesar cipher app. Have a bascic c app setup, printing out the hardcoded string text

---
+++ caesar.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+
+{
+    string text = "Hello World";
+    int key = get_int("Enter a whole number as a key for the cipher:");
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        printf("%c", text[i]);
+    }
+    printf("\n");
+}
+
--- src2/argv2.c
+++ src2/argv2.c
@@ -6,8 +6,10 @@
 
 int main(int argc, string argv[])
 {
+    // iterate over the strings in the argv
     for (int i = 0; i < argc; i++)
     {
+        // iterate over each character in argv[i]
         for (int j = 0, n = strlen(argv[i]); j < n; j++)
         {
             printf("%c\n", argv[i][j]);
