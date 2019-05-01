794f7fcf6ff38aca35327fcc36d47004d69a4daf
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-05-01 23:05:51 +0000

    Completed ceasar cipher. Fully functional, passing all test cases.

--- caesar.c
+++ caesar.c
@@ -5,40 +5,46 @@
 
 int main(int argc, string argv[])
 {
-    if(argc == 1 || argc > 2)
+    if (argc == 1 || argc > 2)
     {
         printf("Usage: ./caesar key\n");
-        return 0;
+        return 1;
     }
+
     int key = atoi(argv[1]);
     string text = get_string("plaintext: ");
 
     printf("ciphertext: ");
     for (int i = 0; i < strlen(text); i++)
     {
-        if( (text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <='Z'))
+        // check to see if each character is alpha, if not it will just be printed without encryption at the final else statement
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
         {
-            char encryptedChar = text[i] + key;
 
-            if (text[i] )
+            // subtract 'a' from your original lower-case char
+            // then add the key
+            // then take %26 of the result
+            // then add 'a' back again
 
-            // printf("%c", encryptedChar);
-            // printf("%c", text[i] + key);
+            // check to see if the character is lower case or uppercase and do some logic to each
+            if (islower(text[i]))
+            {
+                char encryptedChar = (text[i] - 'a' + key) % 26 + 'a';
+                printf("%c", encryptedChar);
+            }
+
+            if (isupper(text[i]))
+            {
+                char encryptedChar = (text[i] - 'A' + key) % 26 + 'A';
+                printf("%c", encryptedChar);
+            }
         }
+
+        // if character is a special symbol and not alpha the character will just be printed as is without encryption
         else
         {
-             printf("%c", text[i]);
+            printf("%c", text[i]);
         }
     }
     printf("\n");
 }
-
-
-
-
-// char c = s[i] + key;
-// if s[i] is a lowercase letter
-//     if c > 'z'
-//         c = 'a' + c -'z' -1
-// else if s[ i ] is upper case letter
-//     . . .
