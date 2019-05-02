95b870ffee96ca57cee7c0c483cb79df9d08c13e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-05-02 02:14:22 +0000

    checking to make sure user only enters one argument on command line when running program.

--- caesar.c
+++ caesar.c
@@ -4,16 +4,14 @@
 
 int main(int argc, string argv[])
 {
-    if(argc == 1)
+    if(argc == 1 || argc > 2)
     {
         printf("Usage: ./caesar key\n");
         return 0;
     }
     int key = atoi(argv[1]);
-    string text = get_string("Enter a word or phrase you would like to ecrypt: ");
-
-
-
+    string text = get_string("plaintext: ");
+    printf("ciphertext: ");
     for (int i = 0; i < strlen(text); i++)
     {
         if( (text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <='Z'))
