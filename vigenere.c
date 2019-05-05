11babc5cb74acb74067485183ae59a63788b42bb
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-05-05 01:31:19 +0000

    cleaned up alpha check if statement to be much more concise

--- vigenere.c
+++ vigenere.c
@@ -23,6 +23,7 @@ int main(int argc, string argv[])
         }
     }
 
+    // for ()
     int key = shift(argv[1][0]);
     // printf("%i\n", key);
 
@@ -32,21 +33,14 @@ int main(int argc, string argv[])
     for (int i = 0; i < strlen(text); i++)
     {
         // check to see if each character is alpha, if not it will just be printed without encryption at the final else statement
-        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+        if (isalpha(text[i]))
         {
-
-            // subtract 'a' from your original lower-case char
-            // then add the key
-            // then take %26 of the result
-            // then add 'a' back again
-
             // check to see if the character is lower case or uppercase and do some logic to each
             if (islower(text[i]))
             {
                 char encryptedChar = (text[i] - 'a' + key) % 26 + 'a';
                 printf("%c", encryptedChar);
             }
-
             if (isupper(text[i]))
             {
                 char encryptedChar = (text[i] - 'A' + key) % 26 + 'A';
