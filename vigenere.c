410c3d4e6fecbb933af27b5fb22f5be7a97bc003
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-05-05 01:22:33 +0000

    combined the new shift function with my old caesar cipher code, and have a encryption working for a one letter user submitted alpha key

--- vigenere.c
+++ vigenere.c
@@ -22,46 +22,46 @@ int main(int argc, string argv[])
             return 1;
         }
     }
-    // printf("success\n");
 
-    // int key = atoi(argv[1]);
-    // string text = get_string("plaintext: ");
-
-    // printf("ciphertext: ");
-    // for (int i = 0; i < strlen(text); i++)
-    // {
-    //     // check to see if each character is alpha, if not it will just be printed without encryption at the final else statement
-    //     if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
-    //     {
-
-    //         // subtract 'a' from your original lower-case char
-    //         // then add the key
-    //         // then take %26 of the result
-    //         // then add 'a' back again
-
-    //         // check to see if the character is lower case or uppercase and do some logic to each
-    //         if (islower(text[i]))
-    //         {
-    //             char encryptedChar = (text[i] - 'a' + key) % 26 + 'a';
-    //             printf("%c", encryptedChar);
-    //         }
-
-    //         if (isupper(text[i]))
-    //         {
-    //             char encryptedChar = (text[i] - 'A' + key) % 26 + 'A';
-    //             printf("%c", encryptedChar);
-    //         }
-    //     }
-
-    //     // if character is a special symbol and not alpha the character will just be printed as is without encryption
-    //     else
-    //     {
-    //         printf("%c", text[i]);
-    //     }
-    // }
-    // printf("\n");
     int key = shift(argv[1][0]);
-    printf("%i\n", key);
+    // printf("%i\n", key);
+
+    string text = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+    for (int i = 0; i < strlen(text); i++)
+    {
+        // check to see if each character is alpha, if not it will just be printed without encryption at the final else statement
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+        {
+
+            // subtract 'a' from your original lower-case char
+            // then add the key
+            // then take %26 of the result
+            // then add 'a' back again
+
+            // check to see if the character is lower case or uppercase and do some logic to each
+            if (islower(text[i]))
+            {
+                char encryptedChar = (text[i] - 'a' + key) % 26 + 'a';
+                printf("%c", encryptedChar);
+            }
+
+            if (isupper(text[i]))
+            {
+                char encryptedChar = (text[i] - 'A' + key) % 26 + 'A';
+                printf("%c", encryptedChar);
+            }
+        }
+
+        // if character is a special symbol and not alpha the character will just be printed as is without encryption
+        else
+        {
+            printf("%c", text[i]);
+        }
+    }
+    printf("\n");
+
 }
 
 
