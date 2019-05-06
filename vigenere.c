732bbf562e388caab2d8c1c7913fffb7ca18e5cb
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-05-06 04:23:22 +0000

    cleaned up some styling to get 100% on submit50

--- vigenere.c
+++ vigenere.c
@@ -2,13 +2,13 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
 
 int shift(char c);
 
 int main(int argc, string argv[])
 {
-     // next two if statements check to see if the user command line input is a single string of alphabetical characters
+    // next two if statements check to see if the user command line input is a single string of alphabetical characters
     if (argc == 1 || argc > 2)
     {
         printf("Usage: ./vigenere keyword\n");
@@ -26,18 +26,22 @@ int main(int argc, string argv[])
     string text = get_string("plaintext: ");
 
     printf("ciphertext: ");
-    for (int i=0, j=0; i < strlen(text); i++)
+    for (int i = 0, j = 0; i < strlen(text); i++)
     {
         // check to see if each character is alpha, if not it will just be printed without encryption at the final else statement
         if (isalpha(text[i]))
         {
             string key = argv[1];
+            // get the length ogf the key
             int key_length = strlen(key);
+            // loop back around to beginning of key when the remainder of (j ÷ the keys length) = 0
             int current_key = j % key_length;
+            // call the shift function to get the correct ASCII code for the alpha key char
             int key_looped = shift(key[current_key]);
             // check to see if the character is lower case or uppercase and do some logic to each
             if (islower(text[i]))
             {
+                // encrypt the character, loop back to begining of alpahbet using modulo if necessary
                 char encryptedChar = (text[i] - 'a' + key_looped) % 26 + 'a';
                 printf("%c", encryptedChar);
             }
@@ -56,7 +60,6 @@ int main(int argc, string argv[])
     }
     printf("\n");
 }
-
 
 int shift(char c)
 {
